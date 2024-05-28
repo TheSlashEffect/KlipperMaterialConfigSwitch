881d899da48915e1cbea8eff5fecd46d4f4edf42
Chris Karampeazis <chriskarampeazis@gmail.com>
2024-05-28 16:48:20 +0200

    Separate config file paths between local and printer environment

--- material_config_switch.py
+++ material_config_switch.py
@@ -5,12 +5,14 @@ import sys
 import shutil
 import logging
 
-# Absolute path, must be under your klipper config directory
+# You can use these entries to run this script locally using the GitHub provided folder structure
+PRINTER_CONFIG_FILE = 'printer.cfg'
 MATERIAL_DIRECTORY = 'MaterialSpecificConfigs/'
+
+# Absolute path, must be under your klipper config directory
 # MATERIAL_DIRECTORY = '/home/fly/klipper_config/MaterialSpecificConfigs/'
 # Absolute path
 # PRINTER_CONFIG_FILE = '/home/fly/klipper_config/printer.cfg'
-PRINTER_CONFIG_FILE = 'printer.cfg'
 PRINTER_CONFIG_FILE_BACKUP_EXTENSION = '.bup'
 MATERIAL_CODE_REGEX = r"[A-Z]{3}\d{3}$"
 MATERIAL_CODE_REGEX_EXAMPLE = 'PLA001'  # Leave empty if you don't want to add an example
