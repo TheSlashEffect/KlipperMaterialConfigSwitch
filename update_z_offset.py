d7f344d8991a3aa2a308957d7664d1e5c8a86c20
Chris Karampeazis <chriskarampeazis@gmail.com>
2024-05-29 18:27:24 +0200

    Use user input as script input

--- update_z_offset.py
+++ update_z_offset.py
@@ -57,7 +57,7 @@ def issue_gcode_command(z_offset_diff):
 
 
 def update_z_offset():
-    input_code = 'PLA001'
+    input_code = common.get_user_input_code()
     new_config_file_location = os.path.join(common.MATERIAL_DIRECTORY, input_code + '.cfg')
     z_offset_diff = clear_and_get_new_config_file_z_offset(new_config_file_location)
     if z_offset_diff == '':
